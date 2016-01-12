09f4114ccf011d22fbb35abf9529e18d2fb2cc4d
Vladimir Zholudev <vzholudev@geeksforless.net>
2016-01-12 22:03:34 +0200

    first commit

---
+++ app/models/customer.rb
@@ -0,0 +1,9 @@
+class Customer < ActiveRecord::Base
+ validates :first_name, presence: true, length: { minimum: 2 }
+ validates :last_name, presence:true, length: {minimum: 2}
+ 
+ def address
+	"#{street} #{zip} #{city}"
+ end
+ 
+end
---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+
+
+Customer.destroy_all
+ 
+ 
+Customer.create!([
+    {
+    	id: 1, first_name: "John", last_name: "Doe", email: "john@doe.com",
+        street: "Allen St.", city: "Manhattan", zip: "10002"
+     },
+     {
+    	id: 2, first_name: "Jane", last_name: "Roe", email: "jane@roe.com",
+        street: "Boo St.", city: "Kirkland", zip: "98034"
+    },
+    {
+    	id: 3, first_name: "Foo", last_name: "Bar", email: "foo@bar.com",
+        street: "Boo St.", city: "Kirkland", zip: "98034"
+    },
+    {
+    	id: 4, first_name: "Rachid", last_name: "Benkirane", email: "contact@roe.com", 
+        street: "Boo St.", city: "Kirkland",zip: "98034"
+    }
+ ])
+ 
+p "Created #{Customer.count} customers."
+
